c817d7630f691254c3be68fdcd81c5ef155327cd
李清龙 <924229679@qq.com>
2022-08-15 08:11:34 +0800

    feat: 逻辑优化

--- src/PageList/components/CardContent/index.tsx
+++ src/PageList/components/CardContent/index.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import type { GetListReponseItem } from '../../api'
+import type { GetListReponseItem } from "../../api";
 
-import './style.css';
+import "./style.css";
 
-const CardContent: React.FC<{ data: GetListReponseItem }> = React.memo(({ data }) => {
-	return <div className="card-container">
-		<span className='content'>
-			{data.title}
-		</span>
-		<span className='time-range'>
-			{data.create_time} - {data.create_time + data.duration}
-		</span>
-	</div>
-})
+const CardContent: React.FC<{ data: GetListReponseItem }> = React.memo(
+  ({ data }) => {
+    return (
+      <div className="card-container">
+        <span className="content">{data.title}</span>
+        <span className="time-range">
+          {data.create_time} - {data.create_time + data.duration}
+        </span>
+      </div>
+    );
+  }
+);
 
 export default CardContent;
